0b639ea2d9915ae56f55a17fb501f9925a009486
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2017-12-05 17:10:56 -0800

    Added a publish script

---
+++ src/publish_new_content.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+allowed_dir="dmtavt.github.io"
+
+if [ ${PWD##*/} != "src" ]; then
+	echo "You must be in 'src' direcotry to execute this script"
+	exit 1
+fi
+
+cd ..
+cur_dir=${PWD##*/}
+
+printf "Upper level dir: %s\n" $cur_dir
+if [ $cur_dir == "$allowed_dir" ]; then
+	echo "Removing old content:"
+	ls | grep -v src 
+	ls | grep -v src | xargs rm -rf
+	cd src
+	echo "Copying new contenst:"
+	ls public/*
+	cp -r public/* ..
+else
+	echo "You must be in $allowed_dir/src to execute this script"
+fi
+
+
+# cp public/* ..
+
